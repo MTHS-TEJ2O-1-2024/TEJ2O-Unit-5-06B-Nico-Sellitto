df64e9756a9a6a1b458ec85ca81bbddb5eaa21a2
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-16 10:35:16 -0400

    changed text

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: Oct 2024
+ * This program uses a sonar and diplays distance
 */
 
-basic.showString('Hello, World!')
