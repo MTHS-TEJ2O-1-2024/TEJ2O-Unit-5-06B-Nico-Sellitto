cf4a7b979bed82867251ea9eabddfdd38f913833
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-16 10:59:57 -0400

    coded

--- main.ts
+++ main.ts
@@ -5,3 +5,21 @@
  * This program uses a sonar and diplays distance
 */
 
+let distancefromobject: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// gets the distance and displays it
+input.onButtonPressed(Button.A, function() {
+
+    basic.clearScreen()
+    distancefromobject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distancefromobject)
+    basic.showIcon(IconNames.Happy)
+    
+})
